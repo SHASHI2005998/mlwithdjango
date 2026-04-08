c2cc0ba5baea1ab81fd48fa49ee768be94303f64
Shashikumar T <sk3332441@gmail.com>
2026-04-08 11:17:05 +0530

    debug on

--- mlwithdjango/settings.py
+++ mlwithdjango/settings.py
@@ -80,7 +80,8 @@ WSGI_APPLICATION = 'mlwithdjango.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
