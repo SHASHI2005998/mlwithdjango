f798ef476389c48da9dddacb1edcc243cc3b4b2c
Shashikumar T <sk3332441@gmail.com>
2026-04-10 00:16:03 +0530

    debug on

--- mlwithdjango/settings.py
+++ mlwithdjango/settings.py
@@ -12,7 +12,13 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+# FIXED
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+
+# IMPORTANT for Render
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.onrender.com"
+]
 
 # ========================
 # 📦 INSTALLED APPS
@@ -69,9 +75,9 @@ TEMPLATES = [
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=not DEBUG
     )
 }
 
