3d5db50ecc939833526530bb8eea40af624ab8d9
Shashikumar T <sk3332441@gmail.com>
2026-04-10 00:38:50 +0530

    debug on

--- ml_model/predictor.py
+++ ml_model/predictor.py
@@ -6,10 +6,18 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(BASE_DIR, "spam_model.pkl")
 vectorizer_path = os.path.join(BASE_DIR, "vectorizer.pkl")
 
-model = pickle.load(open(model_path, "rb"))
-vectorizer = pickle.load(open(vectorizer_path, "rb"))
+def load_model():
+    with open(model_path, "rb") as f:
+        model = pickle.load(f)
+
+    with open(vectorizer_path, "rb") as f:
+        vectorizer = pickle.load(f)
+
+    return model, vectorizer
+
 
 def predict_email(text):
+    model, vectorizer = load_model()
     text_vec = vectorizer.transform([text])
     prediction = model.predict(text_vec)[0]
     return prediction
